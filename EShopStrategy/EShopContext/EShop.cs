5041ef0a859f0a3f8e3b9497908e9cbbbc639c82
Labros Papadimitriou <87263288+Labros-Papadimitriou@users.noreply.github.com>
2022-03-10 02:14:49 +0200

    Add files via upload

---
+++ EShopStrategy/EShopContext/EShop.cs
@@ -0,0 +1,61 @@
+﻿using EShopStrategy.Models;
+using EShopStrategy.PaymentMethodStrategy;
+using EShopStrategy.VariationStrategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopStrategy.EShopContext
+{
+    public class EShop
+    {
+        //Fields
+        private IEnumerable<IVariationStrategy> variations;
+        private IPaymentMethodStrategy paymentMethod;
+
+        public void SetPaymentMethod (IPaymentMethodStrategy method)
+        {
+            paymentMethod = method;
+        }
+
+
+        public void SetVariation(IEnumerable<IVariationStrategy> variationStrategies)
+        {
+            variations = variationStrategies;
+        }
+
+        public void CalculateCost(TShirt shirt)
+        {
+            foreach (var variation in variations)
+            {
+                variation.Cost(shirt);
+            }
+            Console.WriteLine($"T-Shirt final price is {shirt.Price} euros");
+        }
+
+
+        public void PayTShirt(decimal price)
+        {
+            if (paymentMethod.Pay(price))
+            {
+                Console.WriteLine("Transaction was successfull");
+            }
+            else
+            {
+                Console.WriteLine("Transaction aborted");
+            }
+        }
+
+        public void Discount(TShirt shirt, decimal? percentage)
+        {
+            paymentMethod.Discount(shirt, percentage);
+        }
+
+
+
+
+
+    }
+}
---
+++ EShopStrategy/Models/TShirt.cs
@@ -0,0 +1,25 @@
+﻿using EShopStrategy.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopStrategy.Models
+{
+    public class TShirt
+    {
+        public Color Color;
+        public Fabric Fabric;
+        public Size Size;
+        public decimal Price { get; set; }
+
+        public TShirt(Color color, Fabric fabric, Size size)
+        {
+            Color = color;
+            Fabric = fabric;
+            Size = size;
+        }
+
+    }
+}
